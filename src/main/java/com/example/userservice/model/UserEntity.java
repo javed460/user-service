910475c417f26decc257c6c9ace14882aa67ff95
Javed Bagwan <javed460@gmail.com>
2025-12-22 08:04:30 +0530

    added UserEntity

---
+++ src/main/java/com/example/userservice/model/UserEntity.java
@@ -0,0 +1,22 @@
+package com.example.userservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+}
